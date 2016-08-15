325da522ad225acbc32725f0780004f706278dfc
Prawee Wongsa <prawee@hotmail.com>
2016-08-15 15:34:43 +0700

    Initial commit.

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 module.exports = function() {
 	var data = { users: [] }
-	for (var i = 1; i <=100; i++) {
+	for (var i = 1; i <=50; i++) {
 		data.users.push({ id: i, name: 'user' + 1 })
 	}
 	return data
